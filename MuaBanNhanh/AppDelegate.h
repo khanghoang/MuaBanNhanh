416f12c1373ae1bfd447164d3f38f3aa8b12e3c0
Khang Hoang <hoangtrieukhang@gmail.com>
2015-03-30 14:23:59 +0700

    Initial Commit

---
+++ MuaBanNhanh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 3/30/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MuaBanNhanh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 3/30/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
