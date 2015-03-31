80e9be46a21f2a28b136b856f8a7df8c6574414a
Khang Hoang <hoangtrieukhang@gmail.com>
2015-03-31 22:59:24 +0700

    add some categories

---
+++ MuaBanNhanh/ViewControllers/SideMenu/MBNSideMenuCollectionViewCell.m
@@ -0,0 +1,25 @@
+//
+//  MBNSideMenuCollectionViewCell.m
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 3/31/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNSideMenuCollectionViewCell.h"
+
+@interface MBNSideMenuCollectionViewCell()
+
+@property (weak, nonatomic) IBOutlet UILabel *lblCategoryName;
+@property (weak, nonatomic) IBOutlet UIImageView *imageViewCategory;
+
+
+@end
+
+@implementation MBNSideMenuCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end
