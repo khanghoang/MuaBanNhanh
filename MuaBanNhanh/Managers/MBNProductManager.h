b420e6c0721340df395afa119bef24864569045e
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-05 01:55:01 +0700

    add product manager

---
+++ MuaBanNhanh/Managers/MBNProductManager.h
@@ -0,0 +1,15 @@
+//
+//  MBNProductManager.h
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/5/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBNProductManager : NSObject
+
+- (void)getLatestProducts:(void (^) (NSArray *arrProducts))success failure:(void (^)(NSError *error))failure;
+
+@end
---
+++ MuaBanNhanh/Managers/MBNProductManager.m
@@ -0,0 +1,43 @@
+//
+//  MBNProductManager.m
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/5/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNProductManager.h"
+
+@implementation MBNProductManager
+
++ (instancetype)sharedProvider
+{
+    static MBNProductManager *instance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        instance = [[MBNProductManager alloc] init];
+    });
+
+    return instance;
+}
+
+- (void)getLatestProducts:(void (^) (NSArray *arrProducts))success failure:(void (^)(NSError *error))failure {
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:@"http://muabannhanh.com/api/article/latest-list" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSError *error;
+        
+        NSArray *arrayCategories = [MTLJSONAdapter modelsOfClass:[MBNCategory class] fromJSONArray:responseObject[@"result"] error:&error];
+        
+        success(arrayCategories);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        failure(error);
+        
+        
+    }];
+}
+
+@end
