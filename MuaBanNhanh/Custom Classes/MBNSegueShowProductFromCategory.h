2517da4a3557601a8d926c664e865e8af2e7ff27
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-09 11:13:20 +0700

    custom segue from category to product

---
+++ MuaBanNhanh/Custom Classes/MBNSegueShowProductFromCategory.h
@@ -0,0 +1,13 @@
+//
+//  MBNSegueShowProductFromCategory.h
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/9/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNSegueShowProductFromCategory : UIStoryboardSegue
+
+@end
---
+++ MuaBanNhanh/Custom Classes/MBNSegueShowProductFromCategory.m
@@ -0,0 +1,13 @@
+//
+//  MBNSegueShowProductFromCategory.m
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/9/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNSegueShowProductFromCategory.h"
+
+@implementation MBNSegueShowProductFromCategory
+
+@end
