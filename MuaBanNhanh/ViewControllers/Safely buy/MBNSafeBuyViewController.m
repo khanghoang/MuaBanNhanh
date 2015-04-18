e25b29697c66cab0982fd845e0191c032ae008ac
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-19 00:04:03 +0700

    work on 320px width

--- MuaBanNhanh/ViewControllers/Safely buy/MBNSafeBuyViewController.m
+++ MuaBanNhanh/ViewControllers/Safely buy/MBNSafeBuyViewController.m
@@ -51,10 +51,29 @@ UIGestureRecognizerDelegate
 //    if (panGesture.state == UIGestureRecognizerStateEnded) {
 //        
 //    }
+    CGPoint touchLocation = [panGesture translationInView:self.scrollView];
+    NSLog(@"touch = %@, current = %@", NSStringFromCGPoint(touchLocation), NSStringFromCGPoint(self.currentOffset));
+    NSLog(@"velocity = %@", NSStringFromCGPoint([panGesture velocityInView:self.view]));
+    [self.scrollView setContentOffset:CGPointMake(self.currentOffset.x - touchLocation.x, 0) animated:NO];
     
-    CGPoint touchLocation = [panGesture locationInView:self.view];
-    [self.scrollView setContentOffset:CGPointMake(self.currentOffset.x + touchLocation.x, 0) animated:YES];
-    self.currentOffset = self.scrollView.contentOffset;
+    if (panGesture.state == UIGestureRecognizerStateEnded)
+    {
+//        CGPoint velocity = [panGesture velocityInView:self.view];
+//        CGFloat magnitude = sqrt((velocity.x * velocity.x));
+//        CGFloat slideMultiplier = magnitude / 2000;
+//        
+//        CGFloat normalX = self.currentOffset.x - touchLocation.x;
+//        CGFloat finalX = MAX(0, MIN(normalX - (velocity.x * slideMultiplier), self.scrollView.contentSize.width));
+//        CGPoint finalPoint = CGPointMake(finalX, 0);
+        
+        CGPoint velocity = [panGesture translationInView:self.view];
+        CGFloat finalX = velocity.x > 0 ? self.currentOffset.x - 270 : self.currentOffset.x + 270;
+        finalX = MAX(0, MIN(finalX, self.scrollView.contentSize.width - 320));
+        CGPoint finalPoint = CGPointMake(finalX, 0);
+        
+        self.currentOffset = finalPoint;
+        [self.scrollView setContentOffset:finalPoint animated:YES];
+    }
 }
 
 @end
