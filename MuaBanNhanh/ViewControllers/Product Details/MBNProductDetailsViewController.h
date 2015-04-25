d1971029d50c8b0f762a3917bae76f8b9673cbb5
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-25 20:36:45 +0700

    add view controllers for details view

---
+++ MuaBanNhanh/ViewControllers/Product Details/MBNProductDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBNProductDetailsViewController.h
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/25/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNProductDetailsViewController : UIViewController
+
+@end
---
+++ MuaBanNhanh/ViewControllers/Product Details/MBNProductImagesViewController.m
@@ -0,0 +1,37 @@
+//
+//  MBNProductImagesViewController.m
+//  MuaBanNhanh
+//
+//  Created by Khang Hoang Trieu on 4/25/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNProductImagesViewController.h"
+
+@interface MBNProductImagesViewController ()
+
+@end
+
+@implementation MBNProductImagesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
