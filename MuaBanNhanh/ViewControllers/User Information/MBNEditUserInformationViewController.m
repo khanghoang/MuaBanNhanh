8dc91bca3fbecf0854d5655597997dd329e55d11
Khang Hoang <hoangtrieukhang@gmail.com>
2015-04-29 01:37:00 +0700

    toggle keyboard

--- MuaBanNhanh/ViewControllers/User Information/MBNEditUserInformationViewController.m
+++ MuaBanNhanh/ViewControllers/User Information/MBNEditUserInformationViewController.m
@@ -25,6 +25,9 @@ UITextViewDelegate
 @property (strong, nonatomic) UIImage *avatarImage;
 @property (strong, nonatomic) UIImage *coverImage;
 
+@property (weak, nonatomic) UITextField *activeField;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 // 1st section
 @property (weak, nonatomic) IBOutlet UITextField *lblPhoneNumber;
 @property (weak, nonatomic) IBOutlet UITextField *lblName;
@@ -63,6 +66,8 @@ UITextViewDelegate
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    [self registerForKeyboardNotifications];
     
     [[MBNUserManager sharedProvider] getOwnInformation:^(MBNUser *user) {
         [self updateContentWithUser:user];
@@ -322,6 +327,67 @@ UITextViewDelegate
         self.avatarImage = image;
     }
     
+}
+
+#pragma marks - Keyboard
+
+- (void)registerForKeyboardNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification object:nil];
+    
+}
+
+// Called when the UIKeyboardDidShowNotification is sent.
+- (void)keyboardWasShown:(NSNotification*)aNotification
+{
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    UIEdgeInsets contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0);
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        self.scrollView.contentInset = contentInsets;
+        self.scrollView.scrollIndicatorInsets = contentInsets;
+    }];
+    
+    // If active text field is hidden by keyboard, scroll it so it's visible
+    // Your app might not need or want this behavior.
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= kbSize.height;
+    if (!CGRectContainsPoint(aRect, self.activeField.frame.origin) ) {
+        [self.scrollView scrollRectToVisible:self.activeField.frame animated:YES];
+    }
+}
+
+// Called when the UIKeyboardWillHideNotification is sent
+- (void)keyboardWillBeHidden:(NSNotification*)aNotification
+{
+    UIEdgeInsets contentInsets = UIEdgeInsetsZero;
+    [UIView animateWithDuration:0.3 animations:^{
+        self.scrollView.contentInset = contentInsets;
+        self.scrollView.scrollIndicatorInsets = contentInsets;
+    }];
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    self.activeField = textField;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    self.activeField = nil;
+}
+
+- (IBAction)tapOutsideToDismissKeyboard:(id)sender {
+    self.activeField = nil;
+    [self.view endEditing:YES];
 }
 
 @end
