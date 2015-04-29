ab42ac3922ea4f2eeb2ba0c4700478693292e731
Ba Toan <batoan1221@gmail.com>
2015-04-29 14:39:45 +0700

    Added select category module

---
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/MBNSelectCategoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBNSelectCategoryViewController.h
+//  MuaBanNhanh
+//
+//  Created by iSlan on 4/29/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNSelectCategoryViewController : UIViewController
+
+@end
---
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/MBNSelectCategoryViewController.m
@@ -0,0 +1,22 @@
+//
+//  MBNSelectCategoryViewController.m
+//  MuaBanNhanh
+//
+//  Created by iSlan on 4/29/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNSelectCategoryViewController.h"
+
+@interface MBNSelectCategoryViewController ()
+
+@end
+
+@implementation MBNSelectCategoryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
