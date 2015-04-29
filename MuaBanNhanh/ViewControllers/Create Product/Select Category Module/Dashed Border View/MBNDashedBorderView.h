67704cb1c49d23e7055e18d08cfedce1532cfd18
Ba Toan <batoan1221@gmail.com>
2015-04-29 14:40:57 +0700

    Added custom dashed border view class

---
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/Dashed Border View/MBNDashedBorderView.h
@@ -0,0 +1,13 @@
+//
+//  MBNDashedBorderView.h
+//  MuaBanNhanh
+//
+//  Created by iSlan on 4/29/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBNDashedBorderView : UIView
+
+@end
---
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/Dashed Border View/MBNDashedBorderView.m
@@ -0,0 +1,13 @@
+//
+//  MBNDashedBorderView.m
+//  MuaBanNhanh
+//
+//  Created by iSlan on 4/29/15.
+//  Copyright (c) 2015 Khang Hoang Trieu. All rights reserved.
+//
+
+#import "MBNDashedBorderView.h"
+
+@implementation MBNDashedBorderView
+
+@end
