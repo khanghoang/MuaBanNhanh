e636d6938783657ed2e355b64fcb0d1f48df2f47
Ba Toan <batoan1221@gmail.com>
2015-04-29 16:46:27 +0700

    Implement custom dashed border view

--- MuaBanNhanh/ViewControllers/Create Product/Select Category Module/Dashed Border View/MBNDashedBorderView.m
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/Dashed Border View/MBNDashedBorderView.m
@@ -8,6 +8,44 @@
 
 #import "MBNDashedBorderView.h"
 
+@interface MBNDashedBorderView()
+
+@property (strong, nonatomic) CAShapeLayer *border;
+
+@end
+
 @implementation MBNDashedBorderView
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        [self addDashedBorder];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if (self = [super initWithCoder:aDecoder]) {
+        [self addDashedBorder];
+    }
+    return self;
+}
+
+- (void)addDashedBorder
+{
+    _border = [CAShapeLayer layer];
+    _border.fillColor = nil;
+    _border.strokeColor = [UIColor colorFromHexString:@"#EFEFEF"].CGColor;
+    _border.lineWidth = 3.0f;
+    _border.lineDashPattern = @[@10, @10];
+    [self.layer addSublayer:_border];
+}
+
+- (void)layoutSubviews
+{
+    _border.path = [UIBezierPath bezierPathWithRect:self.bounds].CGPath;
+    _border.frame = self.bounds;
+}
 
 @end
