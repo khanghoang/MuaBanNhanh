b87b748d2ee4e4c529888fea17448ac98e081660
Ba Toan <batoan1221@gmail.com>
2015-04-30 15:29:01 +0700

    Implement select categories module

--- MuaBanNhanh/ViewControllers/Create Product/Select Category Module/MBNSelectCategoryViewController.h
+++ MuaBanNhanh/ViewControllers/Create Product/Select Category Module/MBNSelectCategoryViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+@class MBNCreateProductViewModel;
 
 @interface MBNSelectCategoryViewController : UIViewController
+
+- (instancetype)initWithCreateProductViewModel:(MBNCreateProductViewModel *)viewModel;
 
 @end
