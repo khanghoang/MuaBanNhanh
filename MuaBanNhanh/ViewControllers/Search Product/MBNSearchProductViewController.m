a81bbedba22ccb579c68b1b61b7df921e7cc4c68
Ba Toan <batoan1221@gmail.com>
2015-05-11 21:57:47 +0700

    Implement search product view controller

--- MuaBanNhanh/ViewControllers/Search Product/MBNSearchProductViewController.m
+++ MuaBanNhanh/ViewControllers/Search Product/MBNSearchProductViewController.m
@@ -9,6 +9,9 @@
 #import "MBNSearchProductViewController.h"
 #import "MBNSearchProductViewModel.h"
 #import "MBNProvinceManager.h"
+#import "MBNProductCollectionViewCell.h"
+#import "MBNProductCellFactory.h"
+#import "MBNProductDetailsViewController.h"
 #import <RMPickerViewController.h>
 
 typedef NS_ENUM(NSInteger, PickerViewType){
@@ -16,17 +19,24 @@ typedef NS_ENUM(NSInteger, PickerViewType){
     PickerViewTypeProvince
 };
 
-@interface MBNSearchProductViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
+@interface MBNSearchProductViewController ()
+<UIPickerViewDataSource,
+UIPickerViewDelegate,
+UISearchBarDelegate,
+UICollectionViewDelegateFlowLayout,
+UICollectionViewDataSource,
+UICollectionViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIButton *categoryPickerButton;
 @property (weak, nonatomic) IBOutlet UIButton *provincePickerButton;
 @property (weak, nonatomic) IBOutlet UIView *dismissView;
-
-@property (strong, nonatomic) MBNProvince *selectedProvince;
-@property (strong, nonatomic) MBNCategory *selectedCategory;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
 @property (strong, nonatomic) MBNSearchProductViewModel *viewModel;
 @property (strong, nonatomic) RMPickerViewController *reusePickerViewController;
+
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+@property (weak, nonatomic) IBOutlet UICollectionView *productCollectionView;
 
 @end
 
@@ -53,6 +63,22 @@ typedef NS_ENUM(NSInteger, PickerViewType){
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setupButtonSignal];
+    [self observeViewModelSignal];
+    [self registerNibCollectionView];
+}
+
+- (void)observeViewModelSignal {
+    @weakify(self);
+    [[RACObserve(self, viewModel.products) ignore:nil] subscribeNext:^(NSArray *products) {
+        @strongify(self);
+        if (products.count == 0) {
+            self.infoLabel.hidden = NO;
+            self.infoLabel.text = @"Không có sản phẩm";
+        } else {
+            self.infoLabel.hidden = YES;
+            [self.productCollectionView reloadData];
+        }
+    }];
 }
 
 - (void)setupButtonSignal {
@@ -64,7 +90,7 @@ typedef NS_ENUM(NSInteger, PickerViewType){
             @strongify(self);
             NSInteger selectedRow = [rows.lastObject integerValue];
             MBNCategory *selectedCategory = self.viewModel.categories[selectedRow];
-            self.selectedCategory = selectedCategory;
+            self.viewModel.selectedCategory = selectedCategory;
             [sender setTitle:selectedCategory.name forState:UIControlStateNormal];
             sender.tag = selectedRow;
         } sender:sender];
@@ -77,7 +103,7 @@ typedef NS_ENUM(NSInteger, PickerViewType){
             @strongify(self);
             NSInteger selectedRow = [rows.lastObject integerValue];
             MBNProvince *selectedProvince = self.viewModel.provinces[selectedRow];
-            self.selectedProvince = selectedProvince;
+            self.viewModel.selectedProvince = selectedProvince;
             [sender setTitle:selectedProvince.name forState:UIControlStateNormal];
             sender.tag = selectedRow;
         } sender:sender];
@@ -99,6 +125,9 @@ typedef NS_ENUM(NSInteger, PickerViewType){
     [self presentViewController:self.reusePickerViewController animated:YES completion:nil];
 }
 
+- (void)registerNibCollectionView {
+    [self.productCollectionView registerNib:[MBNProductCollectionViewCell nib] forCellWithReuseIdentifier:[MBNProductCollectionViewCell kind]];
+}
 
 #pragma mark - UIPickerViewDatasource
 
@@ -130,5 +159,65 @@ typedef NS_ENUM(NSInteger, PickerViewType){
     }
 }
 
+#pragma mark - UICollectionViewProtocol
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(300, 142);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    MBNProductCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:[MBNProductCollectionViewCell kind] forIndexPath:indexPath];
+    [cell configWithData:self.viewModel.products[indexPath.item]];
+    return cell;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return self.viewModel.products.count;
+}
+
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    MBNProduct *product = self.viewModel.products[indexPath.item];
+    
+    MBNProductDetailsViewController *productVC = [MBNProductDetailsViewController tme_instantiateFromStoryboardNamed:@"ProductDetails"];
+    
+    productVC.productID = product.ID;
+    
+    [self.navigationController pushViewController:productVC animated:YES];
+}
+
+#pragma mark - UISearchBarProtocol
+
+- (BOOL)searchBarShouldBeginEditing:(UISearchBar *)searchBar
+{
+    self.dismissView.hidden = NO;
+    return YES;
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
+{
+    if (!self.viewModel.selectedCategory) {
+        self.infoLabel.hidden = NO;
+        self.infoLabel.text = @"Chưa chọn chuyên mục";
+    } else if (!self.viewModel.selectedProvince) {
+        self.infoLabel.hidden = NO;
+        self.infoLabel.text = @"Chưa chọn tỉnh thành";
+    } else {
+        self.infoLabel.hidden = YES;
+        [self.viewModel searchProductsWithKeyWord:searchBar.text];
+        [self.view endEditing:YES];
+        self.dismissView.hidden = YES;
+    }
+}
+
+- (void)dismissKeyboard:(UIGestureRecognizer *)gestureRecognizer
+{
+    [self.view endEditing:YES];
+    gestureRecognizer.view.hidden = YES;
+}
 
 @end
